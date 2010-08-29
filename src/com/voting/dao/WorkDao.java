ade178c49051c83d83603341c7db2d5742cfea26
ranrarn <ran.ranran@gmail.com>
2010-08-29 14:19:05 +0800

    modified workDao discuss selection order by

--- src/com/voting/dao/WorkDao.java
+++ src/com/voting/dao/WorkDao.java
@@ -106,7 +106,7 @@ public class WorkDao {
 
 	public List<Discuss> findDisusses(String workId) {
 		
-		String sql = "select discuss_commond,discuss_time,works_id,discuss_id,discuss_username,discuss_ip  from discuss_info where discuss_status='1' and works_id=?";
+		String sql = "select discuss_commond,discuss_time,works_id,discuss_id,discuss_username,discuss_ip  from discuss_info where discuss_status='1' and works_id=? order by discuss_time desc";
 		RowMapper<Discuss> mapper = new RowMapper<Discuss>() {
 			public Discuss mapRow(ResultSet rs, int rowNum) throws SQLException {
 				Discuss discuss = new Discuss();
